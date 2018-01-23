2bb4fbed311ffd703ea99c6991c693aaae2c18c5
Lars Eriksson <erikssonlarsb@gmail.com>
2018-01-23 23:04:15 +0100

    Added basic data tables to client overview.

--- hox-client/src/app/routes/overview/overview.component.ts
+++ hox-client/src/app/routes/overview/overview.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+
+import { AuthService } from '../../services/auth/auth.service';
+import { User } from '../../services/auth/payload';
+import { ApiService } from '../../services/api/api.service';
+
+import { Instrument, Order, Trade } from '../../models/index';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css']
 })
-export class OverviewComponent {
+export class OverviewComponent  implements OnInit  {
+  private user: User;
+  instrumentColumns = ['name', 'expiry'];
+  orderColumns = ['instrument', 'side', 'price', 'quantity', 'tradedQuantity', 'status'];
+  tradeColumns = ['instrument', 'side', 'price', 'quantity', 'counterparty'];
+  instruments: MatTableDataSource<Instrument>;
+  orders: MatTableDataSource<Order>;
+  trades: MatTableDataSource<Trade>;
 
-  constructor() { }
+  constructor(private authService: AuthService, private ApiService: ApiService) { }
 
+  ngOnInit(): void {
+    this.user = this.authService.getLoggedInUser();
+
+    this.ApiService.getInstruments()
+      .then((response) => {
+        this.instruments = new MatTableDataSource<Instrument>(response);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+
+    this.ApiService.getOrders()
+      .then((response) => {
+        this.orders = new MatTableDataSource<Order>(response);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+
+    this.ApiService.getTrades()
+      .then((response) => {
+        this.trades = new MatTableDataSource<Trade>(response);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
 }
