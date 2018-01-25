4aa96366a69fb5fee9e13861d048c4c1d0e15b13
Lars Eriksson <erikssonlarsb@gmail.com>
2018-01-25 11:15:38 +0100

    Fixed orderdepths bug where only instruments with active orders were returned.

--- hox-server/handlers/orderdepths.js
+++ hox-server/handlers/orderdepths.js
@@ -1,26 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var Instrument = require('../models/instrument');
 var Order = require('../models/order');
 var OrderDepth = require('../models/orderdepth');
 
-router.get('/', function(req, res){
-  var query = {status: "ACTIVE"};
-  Order.find(query)
-  .populate('instrument')
-  .exec(function(err, orders) {
-    if (err) {
-      res.status(500).json({'error': err});
-    } else {
-      var orderdepths = {};
-      orders.forEach(function(order) {
-        if(!orderdepths[order.instrument._id]) {
-          orderdepths[order.instrument._id] = new OrderDepth(order.instrument);
-        }
-        orderdepths[order.instrument._id].addOrder(order);
+router.get('/', function(req, res) {
+  var orderDepths = {};
+  Instrument.find({})
+    .then(instruments => {
+      instruments.forEach(function(instrument) {
+        orderDepths[instrument._id] = new OrderDepth(instrument);
       });
-      res.json(Object.keys(orderdepths).map(function(key) {return orderdepths[key];})); // Convert map to array
-    }
-  });
+      return Order.find({status: 'ACTIVE'});
+    })
+    .then(orders => {
+      orders.forEach(function(order) {
+        orderDepths[order.instrument].addOrder(order);
+      });
+      res.json(Object.keys(orderDepths).map(function(key) { return orderDepths[key]; })); // Convert map to array
+    })
+    .catch(err => {
+      res.status(500).json({ error : err });
+    });
 });
 
 module.exports = router
