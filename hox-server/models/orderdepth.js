1d352f8bd8e1425164f06875ec5bb09dc6e1accc
Lars Eriksson <erikssonlarsb@gmail.com>
2018-01-28 00:38:23 +0100

    Changed implementation of orderdepth class.

--- hox-server/models/orderdepth.js
+++ hox-server/models/orderdepth.js
@@ -1,21 +1,62 @@
 class OrderDepth {
   constructor(instrument) {
     this.instrument = instrument;
-    this.buy = [];
-    this.sell = [];
+    this.levels = [];
   }
 
   addOrder(order) {
-    var existing = this[order.side.toLowerCase()].find((level) => level.price == order.price);
-    if(existing) {
-      existing.quantity += order.quantity;
-    } else {
-      this[order.side.toLowerCase()].push({quantity: order.quantity, price: order.price});
-    }
+    var existing = this.levels
+      .find((level) => level[order.side.toLowerCase() + 'Price'] == order.price);
 
-    // Sort levels. Buy has highest on top. Sell has lowest on top.
-    this.buy.sort(function(a, b) {return b.price - a.price});
-    this.sell.sort(function(a, b) {return a.price - b.price});
+    if(existing) {
+      existing[order.side.toLowerCase() + 'Quantity'] += order.quantity;
+    } else {
+      var buySide = this.levels.filter(function(level) {
+        return level.buyPrice != null;
+      }).map(function(level) {
+        return {price: level.buyPrice, quantity: level.buyQuantity};
+      });
+      var sellSide = this.levels.filter(function(level) {
+        return level.sellPrice != null;
+      }).map(function(level) {
+        return {price: level.sellPrice, quantity: level.sellQuantity};
+      });
+
+      if(order.side == "BUY") {
+        buySide.push({quantity: order.quantity, price: order.price});
+        buySide.sort(function(levelA, levelB) {return levelB.price - levelA.price});
+      } else {
+        sellSide.push({quantity: order.quantity, price: order.price});
+        sellSide.sort(function(levelA, levelB) {return levelA.price - levelB.price});
+      }
+      var max = Math.max(buySide.length, sellSide.length);
+      var levels = [];
+      for (var i = 0; i < max; i++) {
+        var buyPrice = null;
+        var buyQuantity = null;
+        var sellPrice = null;
+        var sellQuantity = null;
+        if (buySide[i]) {
+          buyPrice = buySide[i].price;
+          buyQuantity = buySide[i].quantity;
+        }
+        if (sellSide[i]) {
+          sellPrice = sellSide[i].price;
+          sellQuantity = sellSide[i].quantity;
+        }
+        levels.push(new Level(buyPrice, buyQuantity, sellPrice, sellQuantity));
+      }
+      this.levels = levels;
+    }
+  }
+}
+
+class Level {
+  constructor(buyPrice, buyQuantity, sellPrice, sellQuantity) {
+    this.buyPrice = buyPrice;
+    this.buyQuantity = buyQuantity;
+    this.sellPrice = sellPrice;
+    this.sellQuantity = sellQuantity;
   }
 }
 
