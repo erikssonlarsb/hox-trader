a912b0f1615868c528073a97992219a784f0dcff
Lars Eriksson <erikssonlarsb@gmail.com>
2018-02-03 22:37:16 +0100

    client: added link back to login after successful registration.

--- hox-client/src/app/routes/overview/overview.component.ts
+++ hox-client/src/app/routes/overview/overview.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 import { ApiService } from '../../services/api/api.service';
 
--- hox-client/src/app/routes/register/register.component.ts
+++ hox-client/src/app/routes/register/register.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router }  from '@angular/router';
 
 import { ApiService } from '../../services/api/api.service';
 
@@ -15,23 +14,24 @@ export class RegisterComponent {
   username: string;
   password: string;
   confirmPassword: string;
+  success: boolean;
   errorMessage: string;
 
 
-  constructor(private apiService: ApiService, private router: Router) { }
+  constructor(private apiService: ApiService) { }
 
   register(): void {
     this.errorMessage = null;
     if(this.password == this.confirmPassword) {
       this.apiService.postRegistration(this.name, this.username, this.password, this.email, this.phone)
         .then(() => {
-          this.router.navigate(['/login']);
+          this.success = true;
         })
         .catch((error) => {
           this.errorMessage = error;
         });
     } else {
-      this.errorMessage = "Passwords doesn't match"
+      this.errorMessage = "Passwords doesn't match."
     }
 
   }
