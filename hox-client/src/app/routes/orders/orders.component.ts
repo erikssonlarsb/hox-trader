fb6ea83bed40f6292d9ae8be457f8ccce8bb63c8
Lars Eriksson <erikssonlarsb@gmail.com>
2018-02-08 23:43:24 +0100

    client: added user column to orders/transactions when logged in as admin.

--- hox-client/src/app/routes/orders/orders.component.ts
+++ hox-client/src/app/routes/orders/orders.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../../services/auth/auth.service';
 import { ApiService } from '../../services/api/api.service';
 
 import { Order } from '../../models/index';
+import { User } from '../../services/auth/payload';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css']
 })
+
 export class OrdersComponent implements OnInit  {
+  user: User;
   orders: Array<Order>;
 
-  constructor(private ApiService: ApiService) { }
+  constructor(private authService: AuthService, private ApiService: ApiService) { }
 
   ngOnInit(): void {
+    this.user = this.authService.getLoggedInUser();
     this.ApiService.getOrders()
       .then((orders) => {
         this.orders = orders;
--- hox-client/src/app/routes/transactions/transactions.component.ts
+++ hox-client/src/app/routes/transactions/transactions.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../../services/auth/auth.service';
 import { ApiService } from '../../services/api/api.service';
 
 import {Trade } from '../../models/index';
+import { User } from '../../services/auth/payload';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.css']
 })
+
 export class TransactionsComponent implements OnInit  {
+  user: User;
   transactions: Array<Trade>;
 
-  constructor(private ApiService: ApiService) { }
+  constructor(private authService: AuthService, private ApiService: ApiService) { }
 
   ngOnInit(): void {
+    this.user = this.authService.getLoggedInUser();
     this.ApiService.getTrades()
       .then((trades) => {
         this.transactions = trades;
