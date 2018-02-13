0439caff920df171c2c14add447061548996a15a
Lars Eriksson <erikssonlarsb@gmail.com>
2018-02-13 14:03:28 +0100

    client: Implemented settlement handling.

--- hox-client/src/app/routes/settlements/settlements.component.ts
+++ hox-client/src/app/routes/settlements/settlements.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Http }  from '@angular/http';
 
 import { AuthService } from '../../services/auth/auth.service';
 import { ApiService } from '../../services/api/api.service';
@@ -9,14 +11,15 @@ import { User } from '../../services/auth/payload';
 @Component({
   selector: 'app-settlements',
   templateUrl: './settlements.component.html',
-  styleUrls: ['./settlements.component.css']
+  styleUrls: ['./settlements.component.css'],
+  providers: [DatePipe]
 })
 
 export class SettlementsComponent implements OnInit  {
   user: User;
   settlements: Array<Settlement>;
 
-  constructor(private authService: AuthService, private ApiService: ApiService) { }
+  constructor(private datePipe: DatePipe, private http: Http, private authService: AuthService, private ApiService: ApiService) { }
 
   ngOnInit(): void {
     this.user = this.authService.getLoggedInUser();
@@ -44,7 +47,23 @@ export class SettlementsComponent implements OnInit  {
       });
   }
 
-  openSwish(id): void {
-    window.open("swish://paymentrequest?token=12345&callbackurl=localhost", "_blank");
+  openSwish(settlement): void {
+    let body = {
+      format: "svg",
+      payee: {value: settlement.counterpartySettlement.user.phone},
+  	  amount: {value: -settlement.amount},
+  	  message: {value: settlement.user.name + " " + this.datePipe.transform(settlement.updateTimestamp)}
+    }
+
+    var wnd = window.open("", "_blank", "width=500, height=500");
+    this.http
+      .post(`${window.location.origin}/qr`, body)
+      .toPromise()
+      .then(function(response) {
+        wnd.document.write(response.text());
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 }
