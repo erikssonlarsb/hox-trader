4b1f41f559d237c9e2c0098eabd7c8c4bbfc5b0e
Lars Eriksson <erikssonlarsb@gmail.com>
2018-02-18 22:40:24 +0100

    client: reroute to / from /login if already logged in.

--- hox-client/src/app/routes/login/login.component.ts
+++ hox-client/src/app/routes/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router }  from '@angular/router';
 
 import { AuthService } from '../../services/auth/auth.service';
@@ -8,18 +8,24 @@ import { AuthService } from '../../services/auth/auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   username: string;
   password: string;
   errorMessage: string;
 
   constructor(private authService: AuthService, private router: Router) { }
 
+  ngOnInit(): void {
+    if(this.authService.isAuthenticated()) {
+      this.router.navigate(['/']);
+    }
+  }
+
   login(): void {
     this.errorMessage = null;
     this.authService.login(this.username, this.password)
       .then(() => {
-        this.router.navigate(['/instruments']);
+        this.router.navigate(['/']);
       })
       .catch((error) => {
         this.errorMessage = error;
