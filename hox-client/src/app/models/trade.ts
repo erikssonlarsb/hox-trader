f5e1dbd2947cf6f455926bdc5b4c46c8f6c029b8
Lars Eriksson <erikssonlarsb@gmail.com>
2018-04-13 23:35:32 +0200

    client: fixed bug with showing settled trades.

--- hox-client/src/app/models/trade.ts
+++ hox-client/src/app/models/trade.ts
@@ -11,6 +11,7 @@ export class Trade {
   side: ORDER_SIDE;
   price: number;
   quantity: number;
+  isSettled: boolean;
   createTimestamp: Date;
   updateTimestamp: Date;
 
@@ -23,6 +24,7 @@ export class Trade {
     this.side = json.side;
     this.price = json.price;
     this.quantity = json.quantity;
+    this.isSettled = json.isSettled;
     this.createTimestamp = json.createTimestamp ? new Date(json.createTimestamp) : null;
     this.updateTimestamp = json.updateTimestamp ? new Date(json.updateTimestamp) : null;
   }
