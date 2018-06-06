360ffdd43134a9036361c5cf1dd2a80f7d05c877
Lars Eriksson <erikssonlarsb@gmail.com>
2018-06-06 20:38:29 +0200

    client: Added Info section.

---
+++ hox-client/src/app/routes/information/information.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-information',
+  templateUrl: './information.component.html',
+  styleUrls: ['./information.component.css']
+})
+export class InformationComponent{
+
+  constructor() { }
+
+}
