d84051fd93e914207f58069410ba4c7913e281b6
Lars Eriksson <erikssonlarsb@gmail.com>
2018-06-11 21:35:59 +0200

    Replaced openssl with node.js cert-gen to be able to verify existing certificate.

---
+++ cert-gen/config.js
@@ -0,0 +1,7 @@
+const commonName = process.env.COMMON_NAME || 'Hox Trader';
+const keyName = process.env.KEY_NAME || 'hoxtrader';
+
+module.exports = {
+  'commonName': commonName,
+  'keyName': keyName
+};
