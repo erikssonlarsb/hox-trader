8766f40e865a4646a19409c765a3e166b10bfe1f
Lars Eriksson <erikssonlarsb@gmail.com>
2018-06-12 21:08:16 +0200

    client: Added classic market view.

--- hox-client/src/app/routes/instruments/instruments.component.ts
+++ hox-client/src/app/routes/instruments/instruments.component.ts
@@ -15,29 +15,64 @@ export class InstrumentsComponent  implements OnInit  {
   indices: Array<Instrument>;
   firstVisit: boolean = true;
   orderDepthFilter: string = null;
+  configOptions: Object;
+  configSpin: boolean = false;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.firstVisit = localStorage.getItem('firstVisit') != null ? JSON.parse(localStorage.getItem('firstVisit')) : true;
-    this.apiService.getOrderDepths(new HttpParams().set('status', 'ACTIVE'))
-      .subscribe(
-        orderDepths => this.orderDepths = orderDepths
-      );
 
-    let instrumentParams = new HttpParams({
+    let orderDepthParams = new HttpParams({
+      fromObject: {
+        'status': 'ACTIVE',
+        '_populate': 'prices'
+      }
+    });
+    this.apiService.getOrderDepths(orderDepthParams)
+    .subscribe(
+      orderDepths => this.orderDepths = orderDepths
+    );
+
+    let indexParams = new HttpParams({
       fromObject: {
         'type': 'Index',
         '_populate': 'prices'
       }
     });
-    this.apiService.getInstruments(instrumentParams)
-      .subscribe(
-        instruments => this.indices = instruments
-      );
+    this.apiService.getInstruments(indexParams)
+    .subscribe(
+      instruments => this.indices = instruments
+    );
+
+    // Get config from local storage, or initialize new if not stored.
+    this.configOptions = JSON.parse(localStorage.getItem("instrumentsConfig"));
+    if (!this.configOptions) {
+      this.configOptions = {
+        'classicView': {
+          value: false,
+          caption: "Classic view",
+          explanation: "Show classic market overview table."
+        }
+      };
+    }
+
+    // Spin config cog for 10 s to raise awareness.
+    if (this.firstVisit) {
+      this.configSpin = true;
+      setTimeout(() => this.configSpin = false, 10000);
+    }
+
+    // Force ngOnDestroy on page refresh (F5).
+    window.onbeforeunload = () => this.ngOnDestroy();
   }
 
   dismissInfo(): void {
     localStorage.setItem('firstVisit', JSON.stringify(false));
+  }
+
+  ngOnDestroy(): void {
+    // Save config to local storage when component is destroyed.
+    localStorage.setItem("instrumentsConfig", JSON.stringify(this.configOptions));
   }
 }
