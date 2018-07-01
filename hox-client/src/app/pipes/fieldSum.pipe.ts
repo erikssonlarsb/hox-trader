d838271c062733e58fcaec54d1a568f893994fbc
Lars Eriksson <erikssonlarsb@gmail.com>
2018-07-01 22:44:13 +0200

    client: Added sum to settlements table.

--- hox-client/src/app/pipes/fieldSum.pipe.ts
+++ hox-client/src/app/pipes/fieldSum.pipe.ts
@@ -3,23 +3,42 @@ import { Injectable, PipeTransform, Pipe } from '@angular/core';
 @Pipe({name: 'fieldSum', pure: false})
 @Injectable()
 export class FieldSumPipe implements PipeTransform  {
-  transform(objects: Array<any>, field: string, filters: any): number|null {
+  transform(objects: Array<any>, field: string, filter: any, type: string = 'Include', positiveOnly: boolean = false): number|null {
     if(!objects) {
       return null;
     } else {
       try {
         objects = objects.filter(object => {
-          for (let field in filters) {
-            if(object[field] != filters[field]) {
-              return false;
-            }
+          if(type == 'Include') {
+            return evaluate(object, filter);
+          } else {
+            return !evaluate(object, filter);
           }
-          return true;  // All filters passed. Include object.
         });
-        return objects.reduce((sum, object) => sum + object[field], 0);
+        return objects.reduce((sum, object) => sum + (positiveOnly ? object[field] > 0 ? object[field] : 0 : object[field]), 0);
       } catch (e) {
         return null;
       }
     }
   }
+}
+
+function evaluate(object: any, filter: any): boolean {
+  if (filter == null) {
+    return true;
+  }
+  if(filter !== Object(filter)) {
+    // Is end-node. Check filter condition.
+    if (object != filter) {
+      return false;
+    }
+  } else {
+    // Has sub-nodes. Recurse.
+    for(let path in filter) {
+      if(!evaluate(object[path], filter[path])) {
+        return false;
+      }
+    }
+  }
+  return true;  // All filters passed. Include object.
 }
